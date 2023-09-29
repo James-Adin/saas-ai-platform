79884328bd7c3e376d5e021592aa814863445b7a
Adinkrah-James-Antwi <Jamesadinkrah15@icloud.com>
2023-09-29 11:13:03 +0800

    folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+const DashboardPage = () => {
+  return <p>Dashboard page (protecteed)</p>;
+};
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,4 @@
+const LandingPage = () => {
+  return <div>landing page (unprotected)</div>;
+};
+export default LandingPage;
