8b06d89d67ef9638ca9c0edcc95b10bd5d0d8959
Adinkrah-James-Antwi <Jamesadinkrah15@icloud.com>
2023-09-29 12:58:30 +0800

    Clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 
 const DashboardPage = () => {
-  return <p>Dashboard page (protecteed)</p>;
+  return (
+    <div>
+      <p>Dashboard page (protecteed)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 export default DashboardPage;
