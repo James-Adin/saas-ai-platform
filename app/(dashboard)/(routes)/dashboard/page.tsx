71d0c729c40f311e435205d759454b0c200ed39e
Adinkrah-James-Antwi <Jamesadinkrah15@icloud.com>
2023-09-29 16:39:26 +0800

    Sidebar rendering

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,11 @@
 import { Button } from '@/components/ui/button';
-import { UserButton } from '@clerk/nextjs';
+
 import Image from 'next/image';
 
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard page (protecteed)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
